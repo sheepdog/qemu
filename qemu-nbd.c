64849041e8eb3a5c9f3a25564926d083b61c6b72
Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
2015-01-22 17:10:10 +0900

    qemu-nbd: initialize progname with error_set_progname()
    Calling error_get_progname() in the context of qemu-nbd can cause
    segmentation fault because qemu-nbd doesn't initialize its progname
    with error_set_progname(). This patch adds the initialization.
    
    Currently, the missing call of error_set_progname() doesn't cause any
    problems because qemu-nbd doesn't use error_get_progname(). This patch
    is a proactive action.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- qemu-nbd.c
+++ qemu-nbd.c
@@ -449,6 +449,7 @@ int main(int argc, char **argv)
     sa_sigterm.sa_handler = termsig_handler;
     sigaction(SIGTERM, &sa_sigterm, NULL);
     qemu_init_exec_dir(argv[0]);
+    error_set_progname(argv[0]);
 
     while ((ch = getopt_long(argc, argv, sopt, lopt, &opt_ind)) != -1) {
         switch (ch) {
